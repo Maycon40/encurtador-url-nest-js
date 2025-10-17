f09a7ecbff58f96e58708e9af5b7a520a9c554fb
Maycon <mayconprogramador@gmail.com>
2025-10-17 11:14:54 -0300

    Add the database connection

---
+++ src/infra/database.ts
@@ -0,0 +1,32 @@
+import { Client } from 'pg';
+
+async function query(queryObject) {
+  const host = process.env.POSTGRES_HOST;
+  const user = process.env.POSTGRES_USER;
+  const password = process.env.POSTGRES_PASSWORD;
+  const database = process.env.POSTGRES_DB;
+  const postgresPort = Number(process.env.POSTGRES_PORT) || 5432;
+
+  const client = new Client({
+    host,
+    user,
+    password,
+    database,
+    port: postgresPort,
+    ssl: ['development', 'test'].includes(process.env.NODE_ENV || "") ? false : true,
+  })
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result
+  } catch (error) {
+    console.error("Erro", error)
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query
+}
