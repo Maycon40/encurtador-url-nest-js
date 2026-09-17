93c9c5b2063ad4f6144235b782f9459d37acc015
Maycon <mayconprogramador@gmail.com>
2026-09-17 08:39:29 -0300

    feat: add refresh auth guard

---
+++ src/modules/auth/guards/refresh-auth.guard.ts
@@ -0,0 +1,47 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import type { Request } from 'express';
+
+@Injectable()
+export class RefreshAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token not provided');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_REFRESH_SECRET,
+      });
+
+      request['payload'] = {
+        id: payload.sub,
+        email: payload.email,
+        sub: payload.sub,
+        iat: payload.iat,
+        exp: payload.exp,
+      };
+      request['refreshToken'] = token;
+      request['sessionId'] = payload.sessionId;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired refresh token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | null {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' && token ? token : null;
+  }
+}
